e0a6e8a114cb9ecc2693c28d69e891128b48f0d6
Masab Haider <masabhaider4@gmail.com>
2025-02-21 00:46:55 +0500

    Add Admin Add Dialog with options to add property, contractor, and design

--- lib/view/Admin/admin_dashboard_view.dart
+++ lib/view/Admin/admin_dashboard_view.dart
@@ -28,7 +28,235 @@ class AdminFloatingActionButton extends StatelessWidget {
     return FloatingActionButton(
       backgroundColor: AppColor.blueColor,
       child: const Icon(IconlyBold.plus, color: AppColor.whiteColor),
-      onPressed: () {},
+      onPressed: () => _showAddOptionsDialog(context),
+    );
+  }
+
+  void _showAddOptionsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => const AdminAddDialog(),
+    );
+  }
+}
+
+class AdminAddDialog extends StatelessWidget {
+  const AdminAddDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: context.mw * 0.9,
+        decoration: BoxDecoration(
+          color: AppColor.whiteColor,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 20,
+              offset: const Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Header
+            Container(
+              padding: EdgeInsets.all(context.mw * 0.04),
+              decoration: BoxDecoration(
+                color: AppColor.blueColor.withOpacity(0.1),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    IconlyBold.plus,
+                    color: AppColor.blueColor,
+                    size: context.mw * 0.06,
+                  ),
+                  SizedBox(width: context.mw * 0.03),
+                  Text(
+                    'Add New',
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: context.mh * 0.022,
+                      fontWeight: FontWeight.w600,
+                      color: AppColor.blueColor,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: Icon(
+                      Icons.close,
+                      color: AppColor.blueColor,
+                      size: context.mw * 0.05,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Options
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: context.mh * 0.02,
+                horizontal: context.mw * 0.04,
+              ),
+              child: Column(
+                children: [
+                  _AddOptionTile(
+                    title: 'Add Property',
+                    subtitle: 'List a new property for sale or rent',
+                    icon: IconlyBold.home,
+                    onTap: () => Navigator.pop(context),
+                  ),
+                  Divider(color: Colors.grey.withOpacity(0.1)),
+                  _AddOptionTile(
+                    title: 'Add Contractor',
+                    subtitle: 'Register a new contractor',
+                    icon: IconlyBold.work,
+                    onTap: () => Navigator.pop(context),
+                  ),
+                  Divider(color: Colors.grey.withOpacity(0.1)),
+                  _AddOptionTile(
+                    title: 'Upload Design',
+                    subtitle: 'Add new LAD approved design',
+                    icon: IconlyBold.document,
+                    onTap: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+            ),
+
+            // Footer
+            Container(
+              padding: EdgeInsets.all(context.mw * 0.04),
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.05),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(
+                      'Cancel',
+                      style: GoogleFonts.aBeeZee(
+                        color: AppColor.greyColor,
+                        fontSize: context.mh * 0.016,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: context.mw * 0.02),
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColor.blueColor,
+                      foregroundColor: AppColor.whiteColor,
+                      elevation: 0,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: context.mw * 0.04,
+                        vertical: context.mh * 0.015,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      'Add New',
+                      style: GoogleFonts.aBeeZee(
+                        fontSize: context.mh * 0.016,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AddOptionTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const _AddOptionTile({
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(8),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: context.mh * 0.015,
+          horizontal: context.mw * 0.02,
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(context.mw * 0.025),
+              decoration: BoxDecoration(
+                color: AppColor.blueColor.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Icon(
+                icon,
+                color: AppColor.blueColor,
+                size: context.mw * 0.055,
+              ),
+            ),
+            SizedBox(width: context.mw * 0.03),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: context.mh * 0.018,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: context.mh * 0.005),
+                  Text(
+                    subtitle,
+                    style: GoogleFonts.aBeeZee(
+                      fontSize: context.mh * 0.014,
+                      color: AppColor.greyColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              IconlyLight.arrow_right_2,
+              color: AppColor.greyColor,
+              size: context.mw * 0.05,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
